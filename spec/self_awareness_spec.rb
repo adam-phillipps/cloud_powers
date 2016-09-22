797f15b5eece24f074f95bc113eede5ed007e736
adam-phillipps <adam.phillipps@gmail.com>
2016-09-21 20:21:04 -0600

    after cerebrum merge

--- spec/self_awareness_spec.rb
+++ spec/self_awareness_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
-require_relative '../neuron_metadata'
+require_relative '../self_awareness'
 
 describe NeuronMetada do
-  include Smash::CloudPowers::NeuronMetada
+  include Smash::CloudPowers::SelfAwareness
 
   it 'should convert non-ruby vars into ruby naming convention' do
     start = 'fake-var'
-    new_var = NeuronMetada.rubyize(start)
+    new_var = SelfAwareness.rubyize(start)
     expect(new_var).to eq('fake_var')
   end
 end
