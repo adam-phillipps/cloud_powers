e526c38ca4ba5fb5b922c4614852d6c3de49d44e
Gerardo Parajeles <gerardo@parajeles.com>
2016-10-24 22:12:41 -0600

    websocket server and client modification. Allows the user to send procs as method callbacks for the ws methods. GP.

--- lib/cloud_powers/synapse/websocket/websocclient.rb
+++ lib/cloud_powers/synapse/websocket/websocclient.rb
@@ -4,32 +4,40 @@ module Smash
   module CloudPowers
     module Synapse
       module WebSocClient
-        def create_websoc_client( host, port )
+
+        def create_websoc_client(opts = {})
+
           EM.run do
 
-            ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://' + host  + ':' + port)
+            @ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://' + opts[:host]  + ':' + opts[:port])
 
-            ws.onopen do
+            open_callback = opts[:on_open] || Proc.new do
               puts "Connected"
             end
 
-            ws.onmessage do |msg, type|
+            @ws.onopen &open_callback
+
+            on_message_callback = opts[:on_message] || Proc.new do |msg, type|
               puts "Received message: #{msg}"
             end
-        
-            ws.onerror do |error|
+
+            @ws.onmessage &on_message_callback
+
+            on_error_callback = opts[:on_error] || Proc.new do |error|
               puts "Error ==> #{error}"
             end
 
-            ws.onclose do |code, reason|
+            @ws.onerror &on_error_callback
+
+            on_close_callback = opts[:on_close] || Proc.new do |code, reason|
               puts "Disconnected with status code: #{code}"
               puts "Disconnected with status message: #{reason}"
             end
 
-            EventMachine.next_tick do
-              ws.send "Hello Server!"
-            end
+            @ws.onclose &on_close_callback
+
           end
+
         end
       end
     end
