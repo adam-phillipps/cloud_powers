575ba5dc4a197bd49be3c2de42a44fc71420bd35
Gerardo Parajeles <gerardo@parajeles.com>
2016-10-25 18:39:21 -0600

    initial specs, you can check if the web socket server is not nil and if we can connect to an existing server.

--- spec/websocclient_spec.rb
+++ spec/websocclient_spec.rb
@@ -1,10 +1,30 @@
 require 'spec_helper'
-require 'dotenv'
 
 describe 'Synapse::WebSocClient' do
-  include Smash::CloudPowers::Zenv
+
+  include Smash::CloudPowers::Synapse::WebSocServer
+  include Smash::CloudPowers::Synapse::WebSocClient
 
   before(:all) do
-    Dotenv.load("#{project_root}/spec/.test.env")
+    @default_config = {host:'127.0.0.1',port:'9090'}
   end
+
+  it '#create_websoc_server() should be not nil' do
+    @websocket_server = create_websoc_server(@default_config)
+    expect(@websocket_server).to be_truthy
+  end
+
+
+  it '#create_websoc_client() should be not nil' do
+    @clients = {}
+    @default_client_configs = {name:'default_client',host:'127.0.0.1',port:'9090'}
+    create_websoc_client(@default_client_configs)
+
+    @default_client = @clients[@default_client_configs[:name]]
+    expect(@default_client).to be_truthy
+
+  end
+
+
+
 end
